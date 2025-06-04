72176bf07a9d4dc0797c621031cedccda6e5735e
Madhuka200044 <157093153+Madhuka200044@users.noreply.github.com>
2025-06-04 12:35:32 +0530

    Update HotelReservationSystem.java

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -4,5 +4,14 @@ import java.time.temporal.ChronoUnit;
 
 public class HotelReservationSystem 
 {
-
+	private static Scanner scanner = new Scanner(System.in);
+    private static List<Room> rooms = new ArrayList<>();
+    private static List<Reservation> reservations = new ArrayList<>();
+    private static int reservationIdCounter = 1000;
+	
+	public static void main(String[] args) 
+	{
+		
+	}
+		
 }
